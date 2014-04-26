d4eac5d3db3361982e33c748a0a029c54f350021
Brandon Evans <admin@brandonevans.org>
2014-04-26 20:16:04 +0000

    Move log with scrolling and HackBU background.

--- js/sendgrid.js
+++ js/sendgrid.js
@@ -1,12 +1,15 @@
 var moves;
+var moves_valid = ['Up', 'Right', 'Down', 'Left'];
 var moving = false;
 var rate_move = 500;
-var rate_refresh = 5000;
+var rate_refresh = 1000;
 
 function clear()
 {
 	moves = [];
 	$.post('php/sendgrid.php', { 'clear': true });
+	$('#log').html('Move log:<br /><br />Game started.<br />');
+	$('#log').css('overflow-y', 'hidden');
 }
 
 function move_feed(feed)
@@ -20,8 +23,17 @@ function move_feed(feed)
 	}
 
 	move = feed.shift();
-	gm.move(move);
 	moves.push(move);
+	gm.move(move.move);
+	$('#log').append(
+		move.from + ' sent move ' + moves_valid[move.move] + '.<br />'
+	);
+
+	if (moves.length >= 24)
+	{
+		alert();
+		$('#log').css('overflow-y', 'scroll');
+	}
 
 	setTimeout(
 		function ()
--- php/sendgrid.php
+++ php/sendgrid.php
@@ -41,12 +41,12 @@ if ($_POST['clear'])
 if ($_POST['read'])
 {
 	$moves = array();
-	$query = 'SELECT `move` FROM `moves` ORDER BY `id`;';
+	$query = 'SELECT `from`, `move` FROM `moves` ORDER BY `id`;';
 	$result = $MYSQLI->query($query);
 
 	while ($move = $result->fetch_assoc())
 	{
-		$moves[] = $move['move'];
+		$moves[] = $move;
 	}
 	$result->close();
 
